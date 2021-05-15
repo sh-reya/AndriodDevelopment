fd12a3836d788fedbaab4b376a59bbce4f2e2c65
SHREYA KUMARI <65662514+sh-reya@users.noreply.github.com>
2021-05-15 11:21:08 +0530

    Create main.kt

---
+++ Meme Sharing App/Day1/main.kt
@@ -0,0 +1,14 @@
+package com.example.sharememe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun shareMeme(view: View) {}
+    fun nextMeme(view: View) {}
