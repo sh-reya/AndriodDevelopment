97c5302659b0795bc85ad1943baf9ebb79d773d3
SHREYA KUMARI <65662514+sh-reya@users.noreply.github.com>
2021-05-23 13:12:29 +0530

    Create main.kt

---
+++ BuzzFeed/Day1/main.kt
@@ -0,0 +1,36 @@
+package com.example.buzzfeed
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), NewsItemClicked {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recyclerView.layoutManager=LinearLayoutManager(this)
+        val items=fetchData()
+        val adapter=NewsListAdapter(items, this)
+        recyclerView.adapter=adapter
+
+    }
+
+    private fun fetchData(): ArrayList<String>
+    {
+        val list=ArrayList<String>()
+        for(i in 0 until 100)
+        {
+            list.add("Item $i")
+
+        }
+        return list
+    }
+
+    override fun onItemClicked(item: String)
+    {
+        Toast.makeText(this, "Clicked item is $item", Toast.LENGTH_LONG).show()
+    }
+}
